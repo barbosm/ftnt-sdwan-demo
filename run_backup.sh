e5da888cb1fff429f98e8d3747de14397b20dc18
draks <draks@loki.local>
2018-01-15 12:00:50 -0200

    initial commit

---
+++ run_backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+version='1.3.1'
+type_='advpn_'
+#type_='sdwan_'
+
+
+device='s01'
+ssh admin@$device.sdw "
+exec backup config ftp /srv/ftp/sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1 root fortinet
+"
+
+device='s02'
+ssh admin@$device.sdw "
+exec backup config ftp /srv/ftp/sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1 root fortinet
+"
+
+device='s03'
+ssh admin@$device.sdw "
+exec backup config ftp /srv/ftp/sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1 root fortinet
+"
+
+device='dc01'
+ssh admin@$device.sdw "
+exec backup config ftp /srv/ftp/sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1 root fortinet
+"
+
+device='dc02'
+ssh admin@$device.sdw "
+exec backup config ftp /srv/ftp/sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1 root fortinet
+"
+
+chmod -R +r *
