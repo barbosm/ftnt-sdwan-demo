69c5b9633aa438d849174e20499544e90d79ac38
draks <barbosm@users.noreply.github.com>
2018-01-15 06:55:22 -0800

    ftp config

--- advpn_configload.sh
+++ advpn_configload.sh
@@ -3,27 +3,27 @@
 version='1.3.1'
 type_='advpn_'
 
-chmod +r /srv/ftp/sdwan/$version/*
+chmod -R +r /srv/ftp/sdwan/$version/*
 
-#device='dc01'
-#ssh admin@$device.sdw "
-#exec restore config ftp sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1
-#y"
+device='dc01'
+ssh admin@$device.sdw "
+exec restore config ftp sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1
+y"
 
-#device='dc02'
-#ssh admin@$device.sdw "
-#exec restore config ftp sdwan/v$version/$device.sdw_$type_$version.conf 192.168.0.1
-#y"
+device='dc02'
+ssh admin@$device.sdw "
+exec restore config ftp sdwan/v$version/$device.sdw_$type_$version.conf 192.168.0.1
+y"
 
-#device='s01'
-#ssh admin@$device.sdw "
-#exec restore config ftp sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1
-#y"
+device='s01'
+ssh admin@$device.sdw "
+exec restore config ftp sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1
+y"
 
-#device='s02'
-#ssh admin@$device.sdw "
-#exec restore config ftp sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1
-#y"
+device='s02'
+ssh admin@$device.sdw "
+exec restore config ftp sdwan/$version/$device.sdw_$type_$version.conf 192.168.0.1
+y"
 
 device='s03'
 ssh admin@$device.sdw "
--- prepare_bastion.sh
+++ prepare_bastion.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-cp hosts /etc/hosts
+# run
+# apt install dnsmasq vsftpd git
 
+# FTP
+cp ftpusers /etc/ftpusers
+cp vsftpd.conf /etc/vsftpd.conf
+service vsftpd restart
+mkdir -p /srv/ftp/sdwan/1.3.1
+mount --bind /root/scripts/ftnt-sdwan-demo/1.3.1 /srv/ftp/sdwan/1.3.1
+
+# SSH
+mkdir /root/.ssh
+cp config /root/.ssh/config
+
+# DNS
+cp hosts /etc/hosts
+mv /etc/dnsmasq.conf /etc/dnsmasq.conf.ori
+cp dnsmasq.conf /etc/dnsmasq.conf
+service dnsmasq restart
