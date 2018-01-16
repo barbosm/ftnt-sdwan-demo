18d1fe19f61b9f0f5f6440e060a2250df731027b
draks <barbosm@users.noreply.github.com>
2018-01-16 07:49:58 -0800

    organizing files

--- bastion_files/prepare_bastion.sh
+++ bastion_files/prepare_bastion.sh
@@ -12,12 +12,11 @@ mount --bind /root/scripts/ftnt-sdwan-demo/1.3.1 /srv/ftp/sdwan/1.3.1
 
 # SSH
 mkdir /root/.ssh
-cp config /root/.ssh/config
+cp ssh_config /root/.ssh/config
 cp authorized_keys /root/.ssh/authorized_keys
 
 cp id_rsa.key_bastion /root/.ssh/id_rsa
 cp id_rsa.pub_bastion /root/.ssh/id_rsa.pub
-
 
 # DNS
 cp hosts /etc/hosts
